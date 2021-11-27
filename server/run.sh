16e3f43f4dce83120c9588d9b98096804eeee8ba
root <wguo6358@gmail.com>
2021-11-27 13:48:34 -0600

    2

--- server/run.sh
+++ server/run.sh
@@ -1,1 +1,4 @@
-nohup uvicorn main:app --host 0.0.0.0 --port 80 --reload &
+nohup uvicorn main:app --host 0.0.0.0 --port 80 --reload > http.log &
+nohup python3 record.py &
+nohup python3 images.py &
+
