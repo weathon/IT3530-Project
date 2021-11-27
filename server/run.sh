025270fedacedb8aea728ab7816bb32e021a0f4f
root <wguo6358@gmail.com>
2021-11-27 16:38:48 -0600

    Remove reload

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,4 @@
-nohup uvicorn main:app --host 0.0.0.0 --port 80 --reload > http.log &
+nohup uvicorn main:app --host 0.0.0.0 --port 80 > http.log &
 nohup python3 record.py &
 nohup python3 images.py &
 
