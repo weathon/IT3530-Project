ff0ed423daa2593af5d22d7a5392d3bf8bbd8597
root <wguo6358@gmail.com>
2021-11-27 00:27:00 -0600

    Fix repeat filename

--- server/record.py
+++ server/record.py
@@ -1,10 +1,9 @@
 audio_script = """ ffmpeg -f alsa  `# * Use alsa [6]` \
         -channels 4 `# Channel numbers are 4 for ps eyes` \
-        -i hw:1,0 a.wav # * Audio input [6]` \
-        -t 65 `# * Not 60 because it might be within a min, and the -t should be placed before filename`\
-        audio/%d/%d/%d/%d.wav """
+        -i hw:1,0 # * Audio input [6]` \
+        -t 65 audio/%d/%d/%d/%d.wav """
 import os, datetime
-if 1:
+while 1:
     time = datetime.datetime.now()
     os.system("mkdir audio/%d/%d/%d -p" % (time.month, time.day, time.hour))
     os.system("mkdir audio/%d/%d/%d -p" % (time.month, time.day, time.hour + 1)) # In case it is right on the time between hours
