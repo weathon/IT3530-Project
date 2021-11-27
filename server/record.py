a1558a4926ee736a1bbf7b0a05c267f7f31795d5
root <wguo6358@gmail.com>
2021-11-27 00:29:39 -0600

    Fix `

--- server/record.py
+++ server/record.py
@@ -1,6 +1,6 @@
 audio_script = """ ffmpeg -f alsa  `# * Use alsa [6]` \
         -channels 4 `# Channel numbers are 4 for ps eyes` \
-        -i hw:1,0 # * Audio input [6]` \
+        -i hw:1,0 `# * Audio input [6]` \
         -t 65 audio/%d/%d/%d/%d.wav """
 import os, datetime
 while 1:
